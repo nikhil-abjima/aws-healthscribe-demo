e17348aa902a6adc6e06228424a681b6ef8774ad
Vivek Gupta <40149209+vivek-cvr@users.noreply.github.com>
2025-01-15 16:06:32 +0530

    Update SideNav.tsx

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -22,37 +22,6 @@ export default function SideNav() {
             text: 'New Conversation',
             href: '/new',
         },
-        { /* { type: 'divider' },
-         {
-            type: 'link',
-            text: 'Generate Audio',
-            href: '/generate',
-        },
-        { type: 'divider' },
-        {
-            type: 'link',
-            text: 'Settings',
-            href: '/settings',
-        },
-        { type: 'divider' },
-        {
-            type: 'link',
-            text: 'AWS HealthScribe',
-            href: 'https://aws.amazon.com/healthscribe',
-            external: true,
-        },
-        {
-            type: 'link',
-            text: 'AWS for Health',
-            href: 'https://aws.amazon.com/health',
-            external: true,
-        },
-        {
-            type: 'link',
-            text: 'Amazon Web Services',
-            href: 'https://aws.amazon.com',
-            external: true,
-        }, */}
 
     ];
 
